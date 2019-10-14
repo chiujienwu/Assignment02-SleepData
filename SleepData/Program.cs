b694f49cbd95baec65916b4c9ceed9be61dcb111
Jerry C <54548815+chiujienwu@users.noreply.github.com>
2019-10-13 23:27:14 -0500

    Add files via upload
    Clone the Lesson-02 branch in Github.  Will be working with SleepData.
    https://github.com/jgrissom/NETDBProgramming/tree/Lesson-02
    The program currently prompts a user if they want to 1) create or 2) parse a data file. The data creation portion of the program is already complete. The user is asked how many full weeks of data is desired and it generates data for that number of weeks and saves it to a text file. The data represents the number of hours of sleep per night for a user-provided number of weeks.
    Your assignment is to complete the TODO in the code. You will need to parse the file and display a ”weekly report” displayed at the system console. You should use string interpolation to complete the task. Start by adjusting the file variable on line #18 to match your file system.
    
    Of course, you will need to figure how the data is formatted in the text file that is generated. To get you started, each line, represents 1 weeks worth of data. The data contains the start date of the week followed by the # of hours of slept for each night (Sunday – Saturday).
    There is a “,” separating the date from the hours of sleep and a “|” is used as a delimiter for each night’s hours of sleep.
    The next slide demonstrates how the report should look. The screen capture on the left is your minimal goal. For double-secret, extra-credit, create the screen on the right.
    In order to receive the double-secret, extra-credit, your report must be formatted EXACTLY like the example shown.

---
+++ SleepData/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace SleepData
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
+            Console.WriteLine("Enter 2 to parse data.");
+            Console.WriteLine("Enter anything else to quit.");
+            // input response
+            string resp = Console.ReadLine();
+
+            // specify path for data file
+            //string file = "/users/jgrissom/downloads/data.txt";
+            string file = "C:\\Users\\JerryChiu\\Documents\\Fall 2019\\Net Database Programming 156-101-10114-20 Jeff Grissom\\Lesson 02\\data.txt";
+
+            if (resp == "1")
+            {
+                // create data file
+
+                // ask a question
+                Console.WriteLine("How many weeks of data is needed?");
+                // input the response (convert to int)
+                int weeks = int.Parse(Console.ReadLine());
+
+                // determine start and end date
+                DateTime today = DateTime.Now;
+                // we want full weeks sunday - saturday
+                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                // subtract # of weeks from endDate to get startDate
+                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+
+                // random number generator
+                Random rnd = new Random();
+
+                // create file
+                StreamWriter sw = new StreamWriter(file);
+                // loop for the desired # of weeks
+                while (dataDate < dataEndDate)
+                {
+                    // 7 days in a week
+                    int[] hours = new int[7];
+                    for (int i = 0; i < hours.Length; i++)
+                    {
+                        // generate random number of hours slept between 4-12 (inclusive)
+                        hours[i] = rnd.Next(4, 13);
+                    }
+                    // M/d/yyyy,#|#|#|#|#|#|#
+                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+                    // add 1 week to date
+                    dataDate = dataDate.AddDays(7);
+                }
+                sw.Close();
+            }
+            else if (resp == "2")
+            {
+                // TODO: parse data file
+                ArrayList sleep = new ArrayList();
+
+                if (File.Exists(file))
+                {
+                    // read data from file
+                    StreamReader sr = new StreamReader(file);
+
+                    while (!sr.EndOfStream)
+                    {
+                        //start to read data file
+                        string line = sr.ReadLine();
+                        // convert string to array
+                        // first splite the line between Start Date of Week and the hours slept each day of week
+                        string[] arr = line.Split(',');
+                        string[] dateArr = arr[0].Split('/');
+                        string[] hours = arr[1].Split('|');
+
+                        int year = Convert.ToInt16(dateArr[2]);
+                        int month = Convert.ToInt16(dateArr[0]);
+                        int date = Convert.ToInt16(dateArr[1]);
+                        DateTime dt = new DateTime(year, month, date);
+
+                        int total = 0;
+
+                        for (int i = 0; i < hours.Length; i++)
+                        {
+                            total = total + Convert.ToInt16(hours[i]);
+                        }
+
+                        float average = (float) total / hours.Length;
+
+                        Console.WriteLine("Week of {0}, {1}, {2}", dt.ToString("MMM"), date, year);
+                        Console.WriteLine(" Su Mo Tu We Th Fr Sa Tot Avg");
+                        Console.WriteLine(" -- -- -- -- -- -- -- --- ---");
+                        Console.WriteLine(" {0,2} {1,2} {2,2} {3,2} {4,2} {5,2} {6,2} {7,3} {8:n1}", hours[0], hours[1], hours[2], hours[3], hours[4], hours[5], hours[6], total, average);
+                        Console.WriteLine();
+
+                    }
+
+                    sr.Close();
+
+                }
+            }
+        }
+    }
+}
